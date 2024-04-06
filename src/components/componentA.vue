9cfc753a0741fb2bd8e50dcc47705074fab0bfd9
VZServer <161551725+VZServer@users.noreply.github.com>
2024-04-06 14:34:35 +0800

    神奇的輸入框

---
+++ src/components/componentA.vue
@@ -0,0 +1,19 @@
+<template>
+    <p>資料綁定(單向綁定)</p>
+    {{ data }}
+
+    <p>資料綁定(雙向綁定)</p>
+    <input type="text" v-model="text" />
+    <p>{{ text }}</p>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+// 資料驅動畫面
+const data = 'Hello Vue'
+const text = ref("雙向綁定!")
+
+</script>
+
+<style scoped></style>
